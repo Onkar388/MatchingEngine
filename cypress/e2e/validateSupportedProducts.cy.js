44fc9f4356ecc33f1b05b2cf9e5f1b125ab14237
QaTester <talha.naeem@volopa.com>
2024-10-17 19:08:24 +0500

    Test project

---
+++ cypress/e2e/validateSupportedProducts.cy.js
@@ -0,0 +1,66 @@
+/// <reference types = "cypress"/>
+
+describe('Test Suite', function () {
+
+    beforeEach(() => {
+        cy.viewport(1440, 1000)
+    })
+
+    it('TC_01 - Validate Supported Products', function () {
+        cy.visit('https://www.matchingengine.com/') //Visit homepage
+        cy.get("img[alt='Home Matching Engine']")
+            .should('be.visible') // logo 
+        cy.get('h1[class*="vc_custom_heading"]')
+            .should('be.visible') //heading
+            .and('contain.text', 'The Matching Engine is an Enterprise Business System for Copyright Management Organizations')
+        //Validate Module Menu option
+        cy.get('.navbar-dropdown [href*="/matching-module/"]') //Matching Module
+            .should('exist')
+            .and('contain.text', 'Matching Module')
+        cy.get('.navbar-dropdown [href*="/repertoire-management-module/"]') //Repertoire Management Module
+            .should('exist')
+            .and('contain.text', 'Repertoire Management Module')
+        cy.get('.navbar-dropdown [href*="/#modern-ingestion"]') //Modern Ingestion
+            .should('exist')
+            .and('contain.text', 'Modern Ingestion')
+        cy.get('.navbar-dropdown [href*="/#usage-module"]') //Usage
+            .should('exist')
+            .and('contain.text', 'Usage')
+        cy.get('.navbar-dropdown [href*="/#distribution-module"]') //Distribution
+            .should('exist')
+            .and('contain.text', 'Distribution')
+        cy.get('.navbar-dropdown [href*="/#members-portal"]') //Members
+            .should('exist')
+            .and('contain.text', 'Members')
+        //Go to Repertoire Management Module
+        cy.get('.navbar-dropdown [href*="/repertoire-management-module/"]') //Repertoire Management Module
+            .should('exist')
+            .and('contain.text', 'Repertoire Management Module').click({ force: true })
+        cy.url().should('include', '/repertoire-management-module/') //validate URL
+        //Validate Additional Features
+        cy.get('h2.vc_custom_heading').eq(1).should('be.visible').and('contain.text', 'Additional Features')
+        cy.get('.vc_active a').eq(0).scrollIntoView()
+            .should('be.visible')
+            .and('contain.text', 'Maintain') //Maintain tab
+        cy.get('.vc_tta-tab').eq(1).scrollIntoView()
+            .should('be.visible')
+            .and('contain.text', 'Products Supported').click() //Product Supported tab
+        cy.get('h3.vc_custom_heading')
+            .contains('There are several types of Product Supported:') //Heading
+            .should('be.visible')
+        //Validate Supported Products
+        cy.get('.wpb_text_column ul li').eq(4)
+            .should('be.visible')
+            .and('contain.text', 'Cue Sheet / AV Work') //Cue Sheet / AV Work
+        cy.get('.wpb_text_column ul li').eq(5)
+            .should('be.visible')
+            .and('contain.text', 'Recording') //Recording
+        cy.get('.wpb_text_column ul li').eq(6)
+            .should('be.visible')
+            .and('contain.text', 'Bundle') //Bundle
+        cy.get('.wpb_text_column ul li').eq(7)
+            .should('be.visible')
+            .and('contain.text', 'Advertisement') //Advertisement 
+
+    })
+})
